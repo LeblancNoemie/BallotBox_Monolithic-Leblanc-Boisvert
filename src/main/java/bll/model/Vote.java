7ab19e9c477063379867cc4fda66a94fd343e3e3
Philippe <philippe.boisvert7125@outlook.com>
2022-02-24 08:42:39 -0500

    J'essai de faire que mes commit fonctionne

--- src/main/java/bll/model/Vote.java
+++ src/main/java/bll/model/Vote.java
@@ -25,6 +25,7 @@ public class Vote {
         System.out.println("Juste pour tester push commit");
         System.out.println("Juste pour tester push commit");
         System.out.println("Juste pour tester push commit");
+        System.out.println("Juste pour tester push commit");
     }
 
     //MÉTHODES UTILITAIRES ->
