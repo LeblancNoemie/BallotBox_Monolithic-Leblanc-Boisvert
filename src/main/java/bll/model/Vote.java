e50f90898bb7ba789b934e8ce75912102d27bd88
Philippe <philippe.boisvert7125@outlook.com>
2022-02-24 08:30:35 -0500

    J'ai changé le id de vote pout qu'il auto increment.

--- src/main/java/bll/model/Vote.java
+++ src/main/java/bll/model/Vote.java
@@ -19,6 +19,11 @@ public class Vote {
         this.id = AUTO_INCREMENT++;
     }
 
+    private void test()
+    {
+        System.out.println("Juste pour tester push commit");
+    }
+
     //MÉTHODES UTILITAIRES ->
     public int getId() {
         return id;
